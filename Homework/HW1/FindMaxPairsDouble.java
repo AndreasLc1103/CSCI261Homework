009b1b7539198d7c0b95b8dce497e8ea1bcc493b
AndreasLc1103 <Andreaslc1103@gmail.com>
2022-09-10 22:19:02 -0400

    finished the testing and debugging needed

--- Homework/HW1/FindMaxPairsDouble.java
+++ Homework/HW1/FindMaxPairsDouble.java
@@ -1,20 +1,22 @@
+package Homework.HW1;
+
 import java.util.Scanner;
 
 // try tc-grd hmwk1-3 FindMaxPairsDouble.java
 public class FindMaxPairsDouble {
 
-    static void merge(int[] arr, int start, int mid, int end) {
+    static void merge(double[] arr, int start, int mid, int end) {
         // finding start and end indexes
         int leftsize = mid - start + 1;
         int rightsize = end - mid;
         // temp arrays
-        int[] leftarr = new int[leftsize];
-        int[] rightarr = new int[rightsize];
+        double[] leftarr = new double[leftsize];
+        double[] rightarr = new double[rightsize];
 
-        for (int i = 0; i < leftarr.length; i++) {
+        for (int i = 0; i < leftsize; i++) {
             leftarr[i] = arr[start + i];
         }
-        for (int i = 0; i < rightarr.length; i++) {
+        for (int i = 0; i < rightsize; i++) {
             rightarr[i] = arr[mid + 1 + i];
         }
 
@@ -23,11 +25,13 @@ public class FindMaxPairsDouble {
         int k = start;
 
         while (i < leftsize && j < rightsize) {
+
             if (leftarr[i] <= rightarr[j]) {
                 arr[k] = leftarr[i];
+                i++;
             } else {
                 arr[k] = rightarr[j];
-                i++;
+                j++;
             }
             k++;
         }
@@ -45,22 +49,24 @@ public class FindMaxPairsDouble {
 
     }
 
-    static void sort(int[] arr, int left, int right) {
+    static void sort(double[] arr, int left, int right) {
         if (left < right) {
-            int middle = left + (right - 1) / 2;
-            sort(arr, left, right);
+            int middle = left + (right - left) / 2;
+
+            sort(arr, left, middle);
+
             sort(arr, middle + 1, right);
             merge(arr, left, middle, right);
         }
     }
 
-    static int[] calculateSums(int[] numbers, int numbersSize) {
+    static double[] calculateSums(double[] numbers, int numbersSize) {
         int n = numbersSize;
         // sums array to hold the number of sums created
-        int[] sumsArray = new int[(int)(n * ((n - 1) / 2))];
+        double[] sumsArray = new double[(int) ((n * (n - 1)) / 2)];
         int sumsIndex = 0;
-        for (int i = 0; i < sumsArray.length -1; i++) {
-            for (int j = i + 1 ; j < sumsArray.length -1; j++) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
                 sumsArray[sumsIndex] = numbers[i] + numbers[j];
                 sumsIndex++;
             }
@@ -68,12 +74,12 @@ public class FindMaxPairsDouble {
         return sumsArray;
     }
 
-    static int findMaxSum(int[] sumsArray, int sumsArraySize) {
-        int max = sumsArray[0];
-        int maxCount = 1;
-        int currentValue;
-        int currentCount = 1;
-        for (int i = 1; i < sumsArray.length; i++) {
+    static double[] findMaxSum(double[] sumsArray, int sumsArraySize) {
+        double max = sumsArray[0];
+        double maxCount = 1;
+        double currentValue;
+        double currentCount = 1;
+        for (int i = 1; i <= sumsArraySize; i++) {
             currentValue = sumsArray[i];
             if (currentValue == sumsArray[i - 1]) {
                 currentCount++;
@@ -81,27 +87,30 @@ public class FindMaxPairsDouble {
                 currentCount = 1;
             }
             if (currentCount > maxCount) {
-                maxCount = currentValue;
+                maxCount = currentCount;
                 max = currentValue;
             }
         }
-        return max;
+        double[] output = new double[2];
+        output[0] = maxCount;
+        output[1] = max;
+        return output;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int inputLength = scanner.nextInt();
-        int[] sumsArray = new int[inputLength];
+        double[] sumsArray = new double[inputLength];
         for (int i = 0; i < sumsArray.length; i++) {
             sumsArray[i] = scanner.nextInt();
         }
         // list of all calculated sums
-        int[] calculatedSums = calculateSums(sumsArray, sumsArray.length);
+        double[] calculatedSums = calculateSums(sumsArray, sumsArray.length);
         // performs merge sort
         sort(calculatedSums, 0, calculatedSums.length - 1);
         // finds max sums
-        int sum = findMaxSum(calculatedSums, calculatedSums.length - 1);
-        System.out.println(sum);
+        double[] out = findMaxSum(calculatedSums, calculatedSums.length - 1);
+        System.out.println((int) out[0] + " " + out[1]);
     }
 
 }
