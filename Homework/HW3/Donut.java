3bffbd5a7e77626919bc5c32d84d0228d9ee4b08
AndreasLc1103 <Andreaslc1103@gmail.com>
2022-10-06 01:03:03 -0400

    psuedocode finished

--- Homework/HW3/Donut.java
+++ Homework/HW3/Donut.java
@@ -1,5 +1,39 @@
 package Homework.HW3;
 
+import java.util.Scanner;
+
+class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
+
 public class Donut {
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        Point[] coordinates = new Point[size];
+    }
+
 }
