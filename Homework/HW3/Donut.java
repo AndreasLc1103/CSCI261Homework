9f4f025ebf61c6f470a75afb15e484baa2ffefed
AndreasLc1103 <Andreaslc1103@gmail.com>
2022-10-07 00:25:28 -0400

    completed problem #1

--- Homework/HW3/Donut.java
+++ Homework/HW3/Donut.java
@@ -2,38 +2,53 @@ package Homework.HW3;
 
 import java.util.Scanner;
 
-class Point {
-    private int x;
-    private int y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-}
-
 public class Donut {
+
+    /**
+     * meanLocation() is responsible for calculating the mean to find
+     * the best suited location to place a donut shop.
+     * 
+     * @return int of the mean calculated value
+     */
+    public static int meanLocation(int[] inputArray) {
+        int sum = 0;
+        for (int i : inputArray) {
+            sum += i;
+        }
+        return (int) Math.floor(sum / inputArray.length);
+    }
+
+    /**
+     * Calculate sum simulates the given equation by problem #2 on the homework.
+     * this allows us to be able to look at the values that are leading to it and
+     * calculate
+     * the given value
+     * 
+     * @param xArray    int array holding input values
+     * @param yArray    int array holding input values
+     * @param arrlength int of the size
+     * @return returns smalles sum
+     */
+    public static int calculateSum(int[] xArray, int[] yArray, int arrlength) {
+        int sum = 0;
+        int bestX = meanLocation(xArray);
+        int betsY = meanLocation(yArray);
+        for (int i = 0; i < arrlength; i++) {
+            sum += (Math.abs(bestX - xArray[i]) + Math.abs(betsY - yArray[i]));
+        }
+        return sum;
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt();
-        Point[] coordinates = new Point[size];
+        int length = scanner.nextInt();
+        int[] xValues = new int[length];
+        int[] yValues = new int[length];
+        for (int i = 0; i < length; i++) {
+            xValues[i] = scanner.nextInt();
+            yValues[i] = scanner.nextInt();
+        }
+        System.out.println(calculateSum(xValues, yValues, length));
     }
 
 }
---
+++ Homework/HW3/problem#2.md
@@ -0,0 +1,16 @@
+
+A.
+    function approachOne(inputArray){
+        int cursor = 1;
+        smallest = [inputArray.length]
+        While(cursor < inputArray.length){
+          for l = cursor in arrayinput l++ {
+                if l arrayinput[l] < smallest[l - 1]{
+                    smallest[l] = arrayinput[l]
+                }
+            cursor++;
+        }
+        
+     }
+B. The Time Complexity for this approach of the intpretation would be O(N^2)
+C. 
